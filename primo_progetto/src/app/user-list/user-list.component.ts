4d60cc8cb36280c31f54b5e682a093c52d240385
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2026-04-08 11:01:14 +0000

    commit finale del secondo progetto

---
+++ primo_progetto/src/app/user-list/user-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css'
+})
+export class UserListComponent {
+  names: string[];
+
+  constructor() {
+    this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
+  }
+}
